7d7081ad548bfcdfad99cff44495e8e56341a179
Ceyda Sayali <25991070+zceydas@users.noreply.github.com>
2020-04-16 13:43:48 +0200

    Syncing

--- src/js/lib/gorilla-mock.js
+++ src/js/lib/gorilla-mock.js
@@ -1,8 +1,15 @@
 export default {
   store: () => {
-    console.log(`Mocking Gorilla's store method`)
+    console.log(`Mocking Gorilla's 'store' method`)
   },
   finish: () => {
-    console.log(`Mocking Gorilla's finish method`)
+    console.log(`Mocking Gorilla's 'finish' method`)
+  },
+  metric: () => {
+    // TODO save local data from experiment/index.js here
+    console.log(`Mocking Gorilla's 'metric' method`)
+  },
+  populateAndLoad: () => {
+    console.log(`Mocking Gorilla's 'populateAndLoad' method`)
   },
 }
